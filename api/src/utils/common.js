f960a0501edb42d944b474af13a61091fd9af52a
Cobelt <paul_emile@hotmail.fr>
2019-06-07 15:53:03 +0200

    utils functions

--- api/src/utils/common.js
+++ api/src/utils/common.js
@@ -1,3 +1,25 @@
+import get from 'lodash.get';
+import set from 'lodash.set';
+
+export const getParam = (req, param) => {
+  if (Array.isArray(param)) {
+    const results = param.map(n => ({ [n]: getParam(req, n) }))
+    return Object.assign({}, ...results);
+  }
+  return get(req, `params.${param}`) || get(req, `query.${param}`) || get(req, `body.${param}`);
+}
+
+export const setLocale = (res, value) => set(res, 'locales', { ...get(res, 'locales'), ...value });
+
+
+export const getLocale = (res, name) => {
+  if (Array.isArray(name)) {
+    const results = name.map(n => ({ [n]: getLocale(res, n) }))
+    return Object.assign({}, ...results);
+  }
+  return get(res, `locales.${name}`);
+}
+
 export function updateLastModifDate(next) {
   try {
       this.updatedAt = Date.now();
