0392be0b76aec03b0783ba485cd568cd0b7f3bb9
Cobelt <paul_emile@hotmail.fr>
2019-07-07 14:39:26 +0200

    Some config from comming Webpack Prod + NodeJS in a docker

--- client/webpack/prod.config.babel.js
+++ client/webpack/prod.config.babel.js
@@ -1,30 +1,34 @@
-// import webpack from 'webpack';
-// import path from 'path';
-//
-// import { HTML, JS, STYL } from './rules';
-// import { cleanPlugin, copyPlugin, HMRPlugin, htmlPlugin } from './plugins';
-//
-// module.exports = {
-//     mode: 'production',
-//     devtool: 'source-map',
-//
-//     entry: {
-//         app: "./src/app.js",
-//     },
-//     output: {
-//         path: "/docs",
-//         filename: "./index.js",
-//     },
-//
-//     devServer: {
-//         inline:true,
-//         port: 3006
-//     },
-//     module: { rules: [HTML, JS, STYL] },
-//     plugins: [
-//         cleanPlugin('../docs'),
-//         copyPlugin('./srcdemo/app.js'),
-//         htmlPlugin,
-//         HMRPlugin({ webpack }),
-//     ],
-// };
+import webpack from 'webpack';
+import path from 'path';
+import nodeExternals from 'webpack-node-externals';
+
+import { HTML, JS, STYL } from './rules';
+import { cleanPlugin, copyPlugin, htmlPlugin, HMRPlugin, CSSPlugin } from './plugins';
+
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, '../dist'),
+    publicPath: '/',
+    filename: '[name].js'
+  },
+  mode: 'production',
+  target: 'node',
+  node: {
+    // Need this when working with express, otherwise the build fails
+    __dirname: false,   // if you don't put this is, __dirname
+    __filename: false,  // and __filename return blank or /
+  },
+  externals: [nodeExternals()], // Need this to avoid error when working with Express
+  module: {
+    rules: [JS,HTML,STYL],
+  },
+  plugins: [
+      cleanPlugin('../dist'),
+      copyPlugin('./src/app.js'),
+      htmlPlugin,
+      CSSPlugin('[name].css'),
+      // HMRPlugin({ webpack }),
+  ],
+}
