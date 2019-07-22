eb1278e5ce64f203fe0f0503097d28a3183cd408
Cobelt <paul_emile@hotmail.fr>
2019-07-22 15:17:19 +0200

    Removed root grid for flex (only puddle in absolute, content and then footer)

--- client/src/constants/equipments/index.js
+++ client/src/constants/equipments/index.js
@@ -3,3 +3,7 @@ export const MOUNTS = 'equipments/group/MOUNTS';
 export const PETS = 'equipments/group/PETS';
 export const EQUIPMENTS = 'equipments/group/EQUIPMENTS';
 export const ALL = 'equipments/group/ALL';
+
+
+export const DETAIL_EQUIPMENT = 'show/details/equipment';
+export const DETAIL_SET = 'show/details/set';
