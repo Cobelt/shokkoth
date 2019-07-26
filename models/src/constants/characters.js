bef9340525110096205dd530330bf2444a957e9b
Cobelt <paul_emile@hotmail.fr>
2019-07-26 15:00:45 +0200

    constants

--- models/src/constants/characters.js
+++ models/src/constants/characters.js
@@ -1,23 +1,2 @@
-const BREEDS = [
-  'Feca',
-  'Osamodas',
-  'Enutrof',
-  'Sram',
-  'Xelor',
-  'Ecaflip',
-  'Eniripsa',
-  'Iop',
-  'Cra',
-  'Sadida',
-  'Sacrieur',
-  'Pandawa',
-  'Roublard',
-  'Zobal',
-  'Steamer',
-  'Eliotrope',
-  'Huppermage',
-  'Ouginak',
-];
-
 const MALE = 'Male';
 const FEMALE = 'Female';
