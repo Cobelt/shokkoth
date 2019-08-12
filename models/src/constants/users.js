9e0ac2ec62226fc3e481dc2893d8c36f2374b9c3
Cobelt <paul_emile@hotmail.fr>
2019-08-12 10:16:58 +0200

    Moved models to another repo

--- models/src/constants/users.js
+++
@@ -1,9 +0,0 @@
-export const ROLES = {
-  SUPER_ADMIN: 'SUPER_ADMIN',
-  ADMIN: 'ADMIN',
-  MODERATOR: 'MODERATOR',
-  USER: 'USER',
-};
-
-export const ENUM = Object.keys(ROLES);
-export const DEFAULT = ROLES.USER;
